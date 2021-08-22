d4eee16ca1cb6bd89e4841ff8306a5dc027f2076
Vitali Korepanov <laugael@yandex.ru>
2021-08-22 17:43:40 +0200

    Feature: change in line conditions to if else

--- src/Task2.js
+++ src/Task2.js
@@ -4,7 +4,14 @@ import { useEffect, useState } from 'react';
 
 
 const updateTicketQuality = (product, sellIn) => {
-  let quality = product.quality + (product.sellIn < 10 && product.sellIn > 0 ? 2 : 1);
+  let quality;
+
+  if (product.sellIn < 10 && product.sellIn > 0) {
+    quality = product.quality + 2;
+  } else {
+    quality = product.quality + 1;
+  }
+
   if (product.sellIn === 0) quality = 0;
 
   return {
@@ -15,7 +22,14 @@ const updateTicketQuality = (product, sellIn) => {
 };
 
 const updateNormalQuality = (product, sellIn) => {
-  let quality = product.quality - (product.sellIn === 0 || product.isSecondHand ? 2 : 1);
+  let quality;
+
+  if (product.sellIn === 0 || product.isSecondHand) {
+    quality = product.quality - 2;
+  } else {
+    quality = product.quality - 1;
+  }
+
   if (quality < 0) quality = 0;
 
   return {
@@ -28,7 +42,8 @@ const updateNormalQuality = (product, sellIn) => {
 export function updateQuality(products) {
   return products.map(
     item => {
-      const sellIn = item.sellIn > 0 ? item.sellIn - 1 : item.sellIn;
+      let sellIn = item.sellIn - 1;
+      if (sellIn < 0) sellIn = 0;
 
       switch (item.type) {
         case 'TICKETS': return updateTicketQuality(item, sellIn);
